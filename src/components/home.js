d281991a37594ac68a25ea50b72910ae20de5c60
Jacob Hein <jacobhein1892@gmail.com>
2021-06-18 13:25:49 -0700

    basic functionality complete, see if I have enough time to clean up the code

--- src/components/home.js
+++ src/components/home.js
@@ -21,9 +21,20 @@ export default function Home(){
   return(
     <div>
     <div className='left-section'>
-      <img id='redCleat' src={redCleat}/>
-      <img id='greenCleat' src={greenCleat}/>
-      <img id='blueCleat' src={blueCleat}/>
+      <div className='red_wrapper'>
+        <img id='redCleat' src={redCleat}/>
+      </div>
+      <div className= 'green_wrapper'>
+        <img id='greenCleat' src={greenCleat}/>
+      </div>
+      <div className='blue_wrapper'>
+        <img id='blueCleat' src={blueCleat}/>
+      </div>
+      
+    </div>
+    
+    <div>
+
     </div>
     <div className='right-section'>
       <h3 id='productName'>Red Flyknit Trainers</h3>

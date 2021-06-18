9ff5a29615ae62fba59473b202be2fe0c8dc33e4
Jacob Hein <jacobhein1892@gmail.com>
2021-06-18 13:46:13 -0700

    general clean up done. Would love to have more time to really make this more compartmentalized, but it is what it is

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { Link } from 'react-scroll'
+import React from 'react'
 import redCleat from './images/redcleat.png'
 import greenCleat from './images/rectangle21.png'
 import blueCleat from './images/image21.png'
@@ -7,35 +6,24 @@ import './home.css'
 
 export default function Home(){
 
-  const [size, setSize] = useState('small')
-  const [color, setColor] = useState('red')
-
   function handleScroll(element){
     const scrollTo = document.getElementById(element)
     scrollTo.scrollIntoView({behavior: "smooth"});
   }
 
-  function handleSizeChange(props){
-    setSize(props)
-  }
   return(
     <div>
-    <div className='left-section'>
-      <div className='red_wrapper'>
-        <img id='redCleat' src={redCleat}/>
+      <div className='left-section'>
+        <div className='red_wrapper'>
+          <img id='redCleat' src={redCleat}/>
+        </div>
+        <div className= 'green_wrapper'>
+          <img id='greenCleat' src={greenCleat}/>
+        </div>
+        <div className='blue_wrapper'>
+          <img id='blueCleat' src={blueCleat}/>
+        </div>
       </div>
-      <div className= 'green_wrapper'>
-        <img id='greenCleat' src={greenCleat}/>
-      </div>
-      <div className='blue_wrapper'>
-        <img id='blueCleat' src={blueCleat}/>
-      </div>
-      
-    </div>
-    
-    <div>
-
-    </div>
     <div className='right-section'>
       <h3 id='productName'>Red Flyknit Trainers</h3>
       <p className='price'>$190.00</p>
@@ -43,55 +31,15 @@ export default function Home(){
       <div className='sizes'>
         <button
         name='small'
-        // onFocus={(e) => {
-        //   handleSizeChange('small')
-        //   e.target.style.background = 'black'
-        //   e.target.style.color = 'white'
-        // }
-        // }
-        // onBlur={(e) => {
-        //   e.target.style.background = '#F1F1F1'
-        //   e.target.style.color = 'black'
-        // }}
         >Small</button>
         <button
         name='medium'
-        // onFocus={(e) => {
-        //   handleSizeChange('medium')
-        //   e.target.style.background = 'black'
-        //   e.target.style.color = 'white'
-        // }
-        // }
-        // onBlur={(e) => {
-        //   e.target.style.background = '#F1F1F1'
-        //   e.target.style.color = 'black'
-        // }}
         >Medium</button>
         <button
         name='large'
-        // onFocus={(e) => {
-        //   handleSizeChange('medium')
-        //   e.target.style.background = 'black'
-        //   e.target.style.color = 'white'
-        // }
-        // }
-        // onBlur={(e) => {
-        //   e.target.style.background = '#F1F1F1'
-        //   e.target.style.color = 'black'
-        // }}
         >Large</button>
         <button
         name='ExtraLarge'
-        // onFocus={(e) => {
-        //   handleSizeChange('medium')
-        //   e.target.style.background = 'black'
-        //   e.target.style.color = 'white'
-        // }
-        // }
-        // onBlur={(e) => {
-        //   e.target.style.background = '#F1F1F1'
-        //   e.target.style.color = 'black'
-        // }}
         >Extra Large</button>
       </div>
       <div className='colors'>
